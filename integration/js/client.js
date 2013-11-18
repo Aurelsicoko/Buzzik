0e2452c707abe53d29607bc284397150b0683d3a
Aurelsicoko <aurelsicoko@gmail.com>
2013-11-18 15:06:01 +0100

    Version stable 1.1
    Intégration graphique du script (room fonctionnel, affichage buzzer)

---
+++ integration/js/client.js
@@ -0,0 +1,194 @@
+var socket = io.connect('http://localhost:1337');
+
+socket.on('connect', function () {
+
+
+  /*if (typeof location.search != 'undefined'){
+    var getRoom = location.search.split('=');
+    if(!isNaN(getRoom) && getRoom!=''){
+      console.log('coucou')
+      room.rejoindreRoom(getRoom);
+    }
+  }*/
+
+
+
+  room.init({
+    player : 'player',
+    buzzer : 'buzzer',
+    affichage : 'rooms',
+    form : 'titleform',
+    titre : 'title',
+    listeAllJoueur: 'listeAllJoueur',
+    listeJoueur : 'listeJoueur',
+    monScore : 'monScore',
+
+    roomListed : function () {
+      $('#'+room.params.affichage).empty();
+      for (i in room.rooms) {
+        if (room.rooms[i] != null) {
+          $('#'+room.params.affichage).append('<div><a href="#" onclick="room.rejoindreRoom(\''+room.rooms[i].id+'\')">' + room.rooms[i].nom + '</a></div>');
+        }
+      }
+    },
+
+    playerListed : function () {
+      
+      console.log(room.monId);
+
+
+      var element = document.getElementById(room.params.listeAllJoueur);
+      if (typeof element != 'undefined' && element != null) {
+        $('#'+room.params.listeAllJoueur).empty();
+        for (i in room.usernames) {
+          if (room.usernames[i] != null) {
+            if (room.usernames[i].room == room.room) {
+              $('#'+room.params.listeAllJoueur).append('Nom : '+room.usernames[i].user+' Point : '+room.usernames[i].point+'<br>');
+            }
+          }
+        }
+      }
+
+      element = document.getElementById(room.params.listeJoueur);
+      if (typeof element != 'undefined' && element != null) {
+        $('#'+room.params.listeJoueur).empty();
+        for (j in room.usernames) {
+          if (room.usernames[j] != null) {
+            if (room.usernames[j].room == room.room && j != room.monId) {
+              $('#'+room.params.listeJoueur).append('Nom : '+room.usernames[j].user+' Point : '+room.usernames[j].point+'<br>');
+            }
+            if (j == room.monId) {
+              console.log($('#'+room.params.monScore));
+              $('#'+room.params.monScore).empty();
+              $('#'+room.params.monScore).append('Point : '+room.usernames[room.monId].point);
+            }
+          }
+        }
+      }
+    },
+
+
+    emitBuzzed : function () {
+      $('#all').load("template/formulaire.html", function () {
+        $('#'+room.params.titre).focus();
+        $('#'+room.params.form).submit(function (e) {
+          e.preventDefault();
+          room.envoiReponse($('#'+room.params.titre).val());
+          return false;
+        });
+      });
+    },
+
+    onBuzzed : function () {
+      //$('#'+room.params.buzzer).remove();
+      var element = document.getElementById(room.params.player);
+      if (typeof element != 'undefined' && element != null) {
+        document.getElementById(room.params.player).pause();
+      }
+    },
+
+    emptyAnswer : function () {
+      alert('Vous devez entrer un titre !');
+    },
+
+    issetAnswer : function () {
+      socket.emit('reponse', $('#'+room.params.titre).val());
+    },
+
+    appendBuzzer : function () {
+      $('#'+room.params.buzzer).remove();
+      $('#all').load("template/buzzer.html");
+      $('link[rel=stylesheet]:last-of-type').attr("href", "css/mobile.css");
+      console.log('refreshScrore')
+      socket.emit('refreshScrore');
+    },
+
+    played : function () {
+      document.getElementById(room.params.player).play();
+    },
+
+    roomAdded : function () {
+      $('#accueil').remove();
+    },
+
+    roomJoined : function () {
+      $('#all').load("template/buzzer.html");
+      $('link[rel=stylesheet]:last-of-type').attr("href", "css/mobile.css");
+      console.log('refreshScrore')
+      socket.emit('refreshScrore');
+    },
+
+    musiqueLoaded : function () {
+      document.getElementById(room.params.player).src = room.numTrack;
+      if (room.etat == "play") {
+        document.getElementById(room.params.player).play();
+        document.getElementById(room.params.player).addEventListener('ended', function() {
+          socket.emit('musiqueFini');
+        });
+      }
+    }
+  }); 
+
+  console.log(typeof location.search);
+  if (typeof location.search != '' && location.search != ''){
+    var getRoom = location.search.split('=');
+    if(!isNaN(getRoom[1]) && getRoom[1]!=''){
+      room.rejoindreRoom(getRoom[1]);
+    }
+  }else{
+    formulaire.afficherFomulaire();
+  }
+
+
+});
+
+
+socket.on('message', function (message) {
+  alert(message);
+});
+
+socket.on('afficherLesRoomsExistante', function (rooms) {
+  room.listeRoom(rooms);
+});
+
+socket.on('roomAjoute', function () {
+  room.roomAjoute();
+});
+
+socket.on('roomRejoin', function () {
+  room.roomRejoint();
+});
+
+socket.on('afficherJoueur', function (usernames, rooms, monId) {
+  room.usernames = usernames;
+  room.room = rooms;
+  room.monId = monId;
+  room.afficherJoueur();
+})
+
+socket.on('prochaineMusique', function(numTrack, etat){
+  room.prochaineMusique(numTrack, etat)
+});
+
+socket.on('buzz', function () {
+  console.log('buzz dans la room');
+  room.onBuzz();
+});
+
+socket.on('valideBuzz', function (a) {
+  console.log(a);
+  room.valideBuzz();
+});
+
+socket.on('afficheBuzzer', function () {
+  room.afficherBuzzer();
+});
+
+socket.on('refreshScrore', function () {
+  console.log('refreshScrore')
+  socket.emit('refreshScrore');
+});
+
+socket.on('roomDelete', function () {
+  document.location.href="index.html"; 
+});
---
+++ integration/js/room.js
@@ -0,0 +1,260 @@
+var room = {
+
+  defaults : {
+    player : '',
+    buzzer : '',
+    affichage : '',
+    form : '',
+    titre : '',
+    listMusiqueUrl: '',
+    listMusiqueTitle : '',
+
+    emitBuzzed : function () {},
+    onBuzzed : function () {},
+    issetAnswer : function () {},
+    emptyAnswer : function () {},
+    appendBuzzer : function () {},
+    played : function () {},
+    roomAdded : function () {},
+    roomJoined : function () {},
+    musiqueLoaded : function () {},
+    roomListed : function () {},
+    playerListed : function () {}
+  },
+
+  init : function (options) {
+    this.params = $.extend(this.defaults, options);
+  },
+
+  listeRoom : function (rooms) {
+    this.rooms = rooms;
+    this.params.roomListed.call(this);
+  },
+
+  rejoindreRoom : function (room){
+    console.log("Room n°"+room);
+    connect(room);
+  },
+
+  roomAjoute : function () {
+    this.params.roomAdded.call(this);
+  },
+
+  roomRejoint : function () {
+    var element = document.getElementById(room.params.player);
+    if (typeof element == 'undefined' || element == null) {
+      this.params.roomJoined.call(this);
+    }
+  },
+
+  afficherJoueur : function () {
+    this.params.playerListed.call(this);
+  },
+
+  emitBuzz : function () {
+    socket.emit('buzz');
+    console.log("emitbuzz");
+  },
+
+  valideBuzz : function () {
+    console.log('exe');
+    this.params.emitBuzzed.call(this);
+  },
+
+  onBuzz : function () {
+    this.params.onBuzzed.call(this);
+  },
+
+  envoiReponse : function (titre) {
+     if(titre == ''){
+        this.params.emptyAnswer.call(this);
+      }else{
+        this.params.issetAnswer.call(this);
+      }
+  },
+
+  afficherBuzzer : function () {
+    var element = document.getElementById(room.params.player);
+    if (typeof element == 'undefined' || element == null) {
+      this.params.appendBuzzer.call(this);
+      this.params.playerListed.call(this);
+    }else{
+      this.params.played(this);
+      this.params.playerListed.call(this);
+    }
+  },
+
+  prochaineMusique : function (numTrack, etat) {
+    this.etat = etat;
+    this.numTrack = numTrack;
+    var element = document.getElementById(this.params.player);
+    if (typeof element != 'undefined' && element != null) {
+      this.params.musiqueLoaded.call(this);
+    }else{
+     this.params.appendBuzzer.call(this);
+    }
+  },
+
+  creerRoom : function () {
+    $("#reglages").remove();
+
+    alert("Créér une room de "+room.params.nbrJoueur+" joueur(s) au nom de "+room.params.nomPartie+" sur "+room.params.nbrChanson+" chansons !");
+
+    /***************************************/
+
+    socket.emit('ajouterRoom', room.params.nomPartie, room.params.nbrJoueur, room.params.nbrChanson);
+
+    /***************************************/
+
+    function GetPlayList(a, b){
+      var playListUrl = a.split(',');
+      var playListTitle = b.split(',');
+      $('body').load("template/player.html", function () {
+        document.getElementById('player').volume = 0.1;
+        $('#player').on('ended', function() {
+          socket.emit('musiqueFini');
+        });
+        socket.emit('playerPret', {url: playListUrl, title: playListTitle});
+      });
+    }
+
+    function GetUrlObjet(o) {
+        for (i in o) {
+            if (typeof(o[i])=="object") {
+              console.log(o[i].preview);
+              console.log(o[i].title);
+              room.params.listMusiqueUrl = room.params.listMusiqueUrl + "," + o[i].preview;
+              room.params.listMusiqueTitle = room.params.listMusiqueTitle + "," + o[i].title;
+            }
+        }
+    }
+
+    DZ.init({
+      appId : '125515',
+      channelUrl : 'http://web-infocom.fr/hetic/htmlcss',
+      player : {
+        onload : function(){}
+      }
+    });
+
+    DZ.api('playlist/589406715', function(response){
+      console.log(response.tracks.data);
+      //Je récupére l'objet contenant la liste objet de chaque musique
+      GetUrlObjet(response.tracks.data);
+      //Je transforme mes bojets en deux tableau urlMP3 et Titre
+      GetPlayList(room.params.listMusiqueUrl, room.params.listMusiqueTitle)
+    });
+  }
+
+}
+  
+
+function connect(room){
+
+      FB.init({
+        appId      : '205528932959370', // App ID
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        xfbml      : true  // parse XFBML
+      });
+
+      // Get url parameters
+      var parameters = window.location.pathname.split("-");
+      var idRoom = room;
+      //var email = parameters[2].replace(".html", "");
+      /*$.ajax({
+        type: "POST",
+        url: "../verifierParty.php",
+        data: { idRoom: idRoom, email: email}
+      }).done(function( data ) {
+        if(data == 1){*/
+          // Continue, all is good
+            FB.getLoginStatus(function(response)
+            {
+              if(response.status === 'connected'){
+              // connected
+              FB.api('/me', function(userInfo) {
+                console.log("Yeah");
+                  // Get all user informations
+                  var emailUser = userInfo.email;
+                  var prenomUser = userInfo.first_name;
+                  var nomUser = userInfo.last_name;
+                  var avatarUser = "http://graph.facebook.com/"+userInfo.username+"/picture";
+                  var paysUser = userInfo.location.name.split(",")[1];
+                  var ageUser = userInfo.birthday;
+
+                  // If the user isn't subscribed, we subscribe him else we connect him
+                  $.post("connect.php", { idRoom: idRoom, emailUser: emailUser, prenomUser: prenomUser, nomUser: nomUser, avatarUser: avatarUser, paysUser: paysUser, ageUser: ageUser }, function(data){
+                      data = eval('(' + data + ')');
+                      if(data.response == "ok" || data.response == "subscribe"){
+                        // Continue, we can show the buzzer
+                        /*data.buzzer = eval('(' + data.buzzer + ')');
+                        var nomBuzzer = data.buzzer.nomBuzzer;
+                        var sonnerieBuzzer = data.buzzer.sonnerieBuzzer;*/
+
+                        var nom = prenomUser+' '+nomUser;
+
+                        socket.emit('rejoindreRoom', room, nom);
+                      }
+                      else{
+                        // There was a problem, stop the script
+                        return;
+                      }
+                   });
+                });
+              }else if(response.status === 'not_authorized'){
+              // not_authorized
+              login();
+              }else{
+              // not_logged_in
+              login();
+              
+              }
+            });
+
+          function login(_self) {
+              FB.login(function(response) {
+                if (response.authResponse) {
+                      // connected
+                    FB.api('/me', function(userInfo) {
+                      console.log("Yeah");
+                      // Get all user informations
+                      var emailUser = userInfo.email;
+                      var prenomUser = userInfo.first_name;
+                      var nomUser = userInfo.last_name;
+                      var avatarUser = "http://graph.facebook.com/"+userInfo.username+"/picture";
+                      var paysUser = userInfo.location.name.split(",")[1];
+                      var ageUser = userInfo.birthday;
+
+                      // If the user isn't subscribed, we subscribe him else we connect him
+                      $.post("connect.php", { idRoom: idRoom, emailUser: emailUser, prenomUser: prenomUser, nomUser: nomUser, avatarUser: avatarUser, paysUser: paysUser, ageUser: ageUser }, function(data){
+                        data = eval('(' + data + ')');
+                        if(data.response == "ok" || data.response == "subscribe"){
+                          // Continue, we can show the buzzer
+                          /*data.buzzer = eval('(' + data.buzzer + ')');
+                          var nomBuzzer = data.buzzer.nomBuzzer;
+                          var sonnerieBuzzer = data.buzzer.sonnerieBuzzer;*/
+                          var nom = prenomUser+' '+nomUser;
+
+                          socket.emit('rejoindreRoom', room, nom);
+                        }
+                        else{
+                          // There was a problem, stop the script
+                          return;
+                        }
+                       });
+                  });
+                } else {
+                      // cancelled
+                  }
+              },{scope: 'email,user_birthday'});
+          }
+
+        /*}
+        else{
+          // Oups, there are a little problem this email isn't associated to this party
+          document.location.href= "./" 
+        }
+      });*/
+  console.log("FB Connect");
+}  
